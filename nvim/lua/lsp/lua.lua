273edb340d93f1499b5033548eb110dcfbb570db
ireina7 <comcx.yang@gmail.com>
2024-12-31 02:59:11 +0800

    add lazyvim to lua lsp path

--- nvim/lua/lsp/lua.lua
+++ nvim/lua/lsp/lua.lua
@@ -2,10 +2,19 @@ local M = {
     on_init = function(client)
         if client.workspace_folders then
             local path = client.workspace_folders[1].name
-            if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+            if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
                 return
             end
         end
+
+        local lazy_path = function()
+            local path = vim.fn.stdpath("data")
+            if type(path) == 'table' then
+                path = path[1]
+            end
+            return require('util/path').path_join(path, "lazy")
+        end
+
 
         client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
             runtime = {
@@ -17,12 +26,14 @@ local M = {
             workspace = {
                 checkThirdParty = false,
                 library = {
-                vim.env.VIMRUNTIME
-                -- Depending on the usage, you might want to add additional paths here.
-                -- "${3rd}/luv/library"
-                -- "${3rd}/busted/library",
+                    vim.env.VIMRUNTIME,
+                    lazy_path(),
+                    -- Depending on the usage, you might want to add additional paths here.
+                    -- "${3rd}/luv/library"
+                    -- "${3rd}/busted/library",
                 }
-                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues
+                -- when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
                 -- library = vim.api.nvim_get_runtime_file("", true)
             }
         })
@@ -39,5 +50,6 @@ local M = {
     }
 }
 
-return M
+M.lazy_path = lazy_path
 
+return M
