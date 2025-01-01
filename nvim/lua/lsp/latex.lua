c745d8c41b2c94e900c376fee6a552d1e49a6553
ireina7 <comcx.yang@gmail.com>
2025-01-02 03:05:53 +0800

    support xetex

--- nvim/lua/lsp/latex.lua
+++ nvim/lua/lsp/latex.lua
@@ -2,9 +2,26 @@ local M = {
     on_attach = require('lsp/lang').on_attach,
     settings = {
         texlab = {
+            bibtexFormatter = "texlab",
             build = {
-                onSave = true,
+                args = { "-pdf", "-xelatex", "-interaction=nonstopmode", "-synctex=1", "%f" },
+                executable = "latexmk",
+                forwardSearchAfter = false,
+                onSave = true
             },
+            chktex = {
+                onEdit = false,
+                onOpenAndSave = true,
+            },
+            diagnosticsDelay = 300,
+            formatterLineLength = 80,
+            forwardSearch = {
+                args = {}
+            },
+            latexFormatter = "latexindent",
+            latexindent = {
+                modifyLineBreaks = false
+            }
         },
     }
 }
